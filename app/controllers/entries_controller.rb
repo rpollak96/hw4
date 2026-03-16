3613c6baa4b6d5f0cfee856afafb9957e5e509c1
rpollak96 <rachael.pollak@kellogg.northwestern.edu>
2026-03-16 00:14:50 +0000

    authorization

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,26 @@
 class EntriesController < ApplicationController
 
   def new
+    if @current_user == nil
+      flash["notice"] = "You must be logged in to add an entry."
+      redirect_to "/login"
+    end
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = session["user_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    if @current_user
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = session["user_id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    else
+      flash["notice"] = "You must be logged in to add an entry."
+      redirect_to "/login"
+    end
   end
 
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,46 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    if @current_user
+      @places = Place.where({ "user_id" => @current_user["id"] })
+    else
+      @places = []
+    end
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
+    if @current_user && @place && @place["user_id"] == @current_user["id"]
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    else
+      # If place doesn't exist or doesn't belong to the user, don't show anything
+      if @place && (@current_user == nil || @place["user_id"] != @current_user["id"])
+        flash["notice"] = "You don't have access to that place."
+        redirect_to "/places"
+        return
+      end
+      @entries = []
+    end
   end
 
   def new
+    if @current_user == nil
+      flash["notice"] = "You must be logged in to add a place."
+      redirect_to "/login"
+    end
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    if @current_user
+      @place = Place.new
+      @place["name"] = params["name"]
+      @place["user_id"] = @current_user["id"]
+      @place.save
+      redirect_to "/places"
+    else
+      flash["notice"] = "You must be logged in to add a place."
+      redirect_to "/login"
+    end
   end
 
 end
